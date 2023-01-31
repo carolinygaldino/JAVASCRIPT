401080dc76edb20f0d1fce8485750690f72d7c86
carolinygaldino <83650298+carolinygaldino@users.noreply.github.com>
2023-01-31 17:14:20 +0000

    Add files via upload

---
+++ JAVASCRIPT/Day1/JS/app.js
@@ -0,0 +1,35 @@
+//1//
+
+let firstName = "Caroliny";
+
+console.log(`Hello ${firstName} !`);
+
+//2//
+
+let myAge = 30;
+let favTv = "The Office";
+
+console.log(
+  `Hello my name is ${firstName} I am ${myAge} years old. My favourite tv serie is ${favTv}.`
+);
+
+//3//
+console.log(Math.round(Math.random() * 50) + 1);
+
+//4//
+
+let breakfast = "Toast";
+let lunch = "Chicken Sandwich";
+let diner = "Sausage and Mash";
+
+console.log(`Breakfast: ${breakfast}`);
+console.log(`Lunch: ${lunch}`);
+console.log(`Diner: ${diner}`);
+
+breakfast = "Cereal";
+lunch = "Pasta";
+diner = "Pizza";
+
+console.log(`Breakfast: ${breakfast}`);
+console.log(`Lunch: ${lunch}`);
+console.log(`Diner: ${diner}`);
